41704d4fbc5246274867659f9f207202770c1583
pervanne69 <samvel.pogosyan2006@gmail.com>
2024-05-02 20:10:33 +0300

    adding project10 and project11

--- project10/main.py
+++ project10/main.py
@@ -1,9 +1,33 @@
-L, T, va, vb = map(int, input().split())
-n = int(input())
-t = [0, 0]
+from math import gcd
 
-for _ in range(n):
-    i, tmp, d = map(int, input().split())
-    t[i - 1] += d
 
-print(int((va * (T - t[0]) + vb * (T - t[1])) / L))
+def main() -> None:
+    """
+    This is the main function and entry point of program
+    :return:
+    """
+    p, q = map(int, input().split())
+    x1, y1 = map(int, input().split())
+    x2, y2 = map(int, input().split())
+
+    dx = x2 - x1
+    dy = y2 - y1
+    g = gcd(p, q)
+
+    if not dx and not dy:
+        print("YES")
+        return
+    if not p and not q or dx % g or dy % g:
+        print("NO")
+        return
+
+    dx //= g
+    dy //= g
+    p //= g
+    q //= g
+
+    print("YES" if (p & 1 ^ q & 1 or not ((dx + dy) & 1)) else "NO")
+
+
+if __name__ == "__main__":
+    main()
--- project6/main.py
+++ project6/main.py
@@ -1,4 +1,4 @@
 first_september_day = input()
 day_of_first_match = input().split()
-A, B = map(int, input().split())
+list_d, B = map(int, input().split())
 
